2729f2b0cca921c0a59a4964a72a3c563c0d5d23
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-07-09 12:46:21 +0530

    Bug Fixes

--- Source/TypeScript/BigInt.ts
+++ Source/TypeScript/BigInt.ts
@@ -19,14 +19,16 @@ export class BigIntV2 {
 		}
 
 		else if(typeof input === "number") {
-			if(input >= Number.MAX_SAFE_INTEGER || input <= Number.MIN_SAFE_INTEGER) throw new TypeError("If you want to enter a number outside the bounds of the normal number, please provide it as a string");
-			this.chunks = String(input).split("").reverse().map(Number);
+			// if(input > Number.MAX_SAFE_INTEGER || input <= Number.MIN_SAFE_INTEGER) throw new TypeError("If you want to enter a number outside the bounds of the normal number, please provide it as a string");
+			input = this.noExponents(input);
+			this.chunks = input.toString().split("").reverse().map(Number);
 		}
 
 		else if(typeof input === "string") {
-			const parse = parseInt(input);
-			if(isNaN(parse)) throw new TypeError("Invalid string value provided to convert");
+			if(isNaN(Number(input))) throw new TypeError("Input is not a valid number expression");
+			if(!Number.isFinite(Number(input))) throw new RangeError("Infinity can't be an argument");
 
+			if(input.split(/e/i)[1]) input = this.noExponents(input);
 			input = input.split(".")[0];
 
 			this.chunks = String(input).split("").reverse().map(Number);
@@ -40,4 +42,26 @@ export class BigIntV2 {
 	public toString() {
 		return `${this.sign === -1 ? "-" : ""}${this.chunks.reverse().join("")}`;
 	}
+
+	private noExponents(number: number | string) {
+		const data = String(number).split(/e/i);
+		let z = "";
+		const sign = number < 0 ? "-" : "";
+		const str = data[0].replace(".", "");
+		let mag = Number(data[1]) + 1;
+
+		if(mag < 0) {
+			z = sign + "0.";
+			while(mag++) z += "0";
+			return z + str.replace(/^\-/, "");
+		}
+
+		mag -= str.length;
+
+		while(mag--) z += "0";
+
+		return str + z;
+	}
 }
+
+console.log(new BigIntV2(10E100).toString());
